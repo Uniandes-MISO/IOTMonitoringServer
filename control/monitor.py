cff13429a1614272abab312ec4b33e2286550ae1
wilson-bg <w.beltran@uniandes.edu.co>
2022-09-18 18:56:09 -0500

    menor

--- control/monitor.py
+++ control/monitor.py
@@ -61,7 +61,7 @@ def analyze_data():
     
     print("Calculando nuevas alertas...")
     data_new = Data.objects.filter(
-        base_time__gte=datetime.now() - timedelta(hours=1))
+        base_time__gte=datetime.now() - timedelta(minutes=10))
     aggregation_new = data_new.annotate(check_value_min=Min('min_value'), check_value_max=Max('max_value') ) \
         .select_related('station', 'measurement') \
         .select_related('station__user', 'station__location') \
@@ -102,7 +102,7 @@ def analyze_data():
         alert = True
 
         # if alert:
-        message = "*ALERT* {} {} ".format(variable, 1)
+        message = "*ALERT* {} {} ".format(variable, diff)
         topic = '{}/{}/{}/{}/in'.format(country, state, city, user)
         print("Sending alert to {} {}".format(topic, variable))
         client.publish(topic, message)
